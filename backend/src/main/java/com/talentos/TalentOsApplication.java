e9d6ad1c6ad2e19382ef7b8ab8c41e30d43f33fd
Praveenmaila <praveenmaila249@gmail.com>
2026-07-22 20:50:29 +0530

    feat: implement company entity

--- backend/src/main/java/com/talentos/TalentOsApplication.java
+++ backend/src/main/java/com/talentos/TalentOsApplication.java
@@ -2,10 +2,9 @@ package com.talentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class TalentOsApplication {
 
     public static void main(String[] args) {
